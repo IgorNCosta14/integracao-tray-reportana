3dc4150287f03ee4fbd6c6185533cbb3fa55132b
Igor Nascimento Costa <69372874+IgorNCosta14@users.noreply.github.com>
2023-02-09 01:40:47 -0300

    Update server.ts

--- api/src/server.ts
+++ api/src/server.ts
@@ -2,12 +2,16 @@ import express, { Request, Response } from "express";
 
 const app = express()
 
-app.post('/produtos/purchase', (req, res: Response) => {
+app.post('/produtos/purchase', (req: Request, res: Response) => {
     const { seller_id , scope_id, scope_name ,act , update, app_code, url_notification } = req.body;
 
-    
     console.log(seller_id , scope_id, scope_name ,act , update, app_code, url_notification);
 
+    return res.status(201).send();
+})
+
+app.get('/test', (req: Request, res: Response) => {
+    return res.status(201).send("oi")
 })
 
 app.listen(3000, () => {
