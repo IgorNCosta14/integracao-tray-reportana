980dc2a71ce41c3b84ac396d81e25fe19daf9ad5
Igor Nascimento Costa <igorncosta14@hotmail.com>
2023-02-10 15:14:35 -0300

    Update server.ts

--- api/src/server.ts
+++ api/src/server.ts
@@ -5,9 +5,8 @@ const port = process.env.PORT || 8080;
 const app = express()
 
 app.post('/products/purchase', (req: Request, res: Response) => {
-    const { seller_id , scope_id, scope_name ,act , update, app_code, url_notification } = req.body;
 
-    console.log(seller_id , scope_id, scope_name ,act , update, app_code, url_notification);
+    console.log(req);
 
     return res.status(201).send();
 })
