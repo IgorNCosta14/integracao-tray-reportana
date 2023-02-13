0dd4e8eb8e8e1fc2099b05c59ff8eba3ca859a13
Igor Nascimento Costa <igorncosta14@hotmail.com>
2023-02-13 16:03:36 -0300

    Update server.ts

--- api/src/server.ts
+++ api/src/server.ts
@@ -53,7 +53,11 @@ app.post('/products/purchase', async (req: Request, res: Response) => {
           'Content-Type': 'application/json'
         }
     }).then(async (response) => {
-        return await axios.get(`https://391250.commercesuite.com.br/web_api/orders/${req.body.scope_id}?${response.data.access_token}`)
+        return await axios.get(`https://391250.commercesuite.com.br/web_api/orders/${req.body.scope_id}?${response.data.access_token}`).then((response) => {
+            return response;
+        }).catch((error) => {
+            console.log(error);
+        })
     }).catch((error) => {
         console.log(error);
     })
