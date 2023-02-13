3f1ff7649fd30ea938c95d128622f87c447517cb
Igor Nascimento Costa <igorncosta14@hotmail.com>
2023-02-13 13:47:28 -0300

    Update server.ts

--- api/src/server.ts
+++ api/src/server.ts
@@ -8,11 +8,43 @@ const app = express()
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+interface IClient {
+    consumer_key: string,
+    consumer_secret: string,
+    code: string
+}
+
+async function getClient(id: any): Promise<IClient> {  
+    let client: IClient = {
+        consumer_key: "",
+        consumer_secret: "",
+        code: ""
+    }
+
+    switch (`${id}`) {
+        case "391250" : client = {
+            consumer_key: `${process.env.CONSUMERKEY391250}`,
+            consumer_secret: `${process.env.CONSUMERSECRET391250}`,
+            code: `${process.env.CODE391250}`
+        }; break;
+
+        case "391251" : client = {
+            consumer_key: `${process.env.CONSUMERKEY391251}`,
+            consumer_secret: `${process.env.CONSUMERSECRET391251}`,
+            code: `${process.env.CODE391251}`
+        }; break;
+    }
+
+    return client;
+}
+
 app.post('/products/purchase', async (req: Request, res: Response) => {
 
     console.log(req.body);
-    const client = `process.env.CLIENT${req.body.seller_id}`;
-    console.log(`${client}`)
+
+    const client = getClient(req.body.seller_id);
+
+    console.log(client);
 
     return res.status(201).send();
 })
