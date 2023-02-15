23fa9f1bc3456e0276e8c6c94bddb4d8a7a0fbe1
Igor Nascimento Costa <igorncosta14@hotmail.com>
2023-02-15 15:56:49 -0300

    Update Utils.ts

--- api/src/utils/Utils.ts
+++ api/src/utils/Utils.ts
@@ -49,7 +49,12 @@ export async function convertPurchasePaymentStatus(Payment: any[]) {
 export async function convertPurchasePaymentType(codigo: string) {
   console.log(codigo)
 
-  if ( parseInt(codigo) === 10516 || 10526 || 10536 || 10617 || 10623 || 10633 || 10710 || 10725 || 10741 || 10743 ) {
+  console.log(codigo === "10516")
+  console.log(parseInt(codigo) === 10516)
+
+  //|| 10526 || 10536 || 10617 || 10623 || 10633 || 10710 || 10725 || 10741 || 10743
+
+  if ( codigo === "10516" ) {
     return 'PIX';
   } else {
     return 'OTHER';
